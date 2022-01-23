2a8f79b258daeb1eeb60c8fc2e63a40f4a657a24
yushuwu <wu.yushu1@northeastern.edu>
2022-01-23 01:30:26 -0800

    Checking in Hw1

---
+++ Homework1/src/edu/northeastern/yushu/Main.java
@@ -0,0 +1,23 @@
+package edu.northeastern.yushu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str ="ABCD";
+        System.out.println(reverseString(str));
+    }
+    private static String reverseString(String str){
+        char[] arr =str.toCharArray();
+        int start =0;
+        int end =arr.length-1;
+        while (start<=end){
+            char temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        return  String.valueOf(arr);
+    }
+}
