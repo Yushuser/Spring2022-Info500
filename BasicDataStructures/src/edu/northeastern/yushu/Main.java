623aedc52086fd2a9bf90af0a5460e1aa951b267
yushuwu <wu.yushu1@northeastern.edu>
2022-01-23 01:23:26 -0800

    Checking in code for HW1

---
+++ BasicDataStructures/src/edu/northeastern/yushu/Main.java
@@ -0,0 +1,16 @@
+package edu.northeastern.yushu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean boolValue = true;
+        int iValue =5;
+        char cValue ='a';
+        float fValue=3.45f;
+        Integer bigIntValue =45;
+        Character ch='b';
+        String str="I am a string";
+        System.out.println(Short.MAX_VALUE);
+    }
+}
