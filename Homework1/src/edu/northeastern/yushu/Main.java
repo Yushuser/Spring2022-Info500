201b55cba2e3299a52f40913df52756d550e9639
yushuwu <wu.yushu1@northeastern.edu>
2022-01-30 23:09:19 -0800

    Committing again

--- Homework1/src/edu/northeastern/yushu/Main.java
+++ Homework1/src/edu/northeastern/yushu/Main.java
@@ -3,21 +3,36 @@ package edu.northeastern.yushu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        String str ="ABCD";
+        String str = "ABCD";
         System.out.println(reverseString(str));
+
+        System.out.println(base64(12345678912));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
     }
+    // Question 1 : Reverse a string
     private static String reverseString(String str){
-        char[] arr =str.toCharArray();
-        int start =0;
-        int end =arr.length-1;
-        while (start<=end){
-            char temp=arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp;
-            start++;
-            end--;
+        char[] arr = str.toCharArray();
+        int start = 0 ;
+        int end = arr.length -1;
+
+        while(start <= end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start ++;
+            end --;
         }
-        return  String.valueOf(arr);
+        return String.valueOf(arr);
+    }
+
+    private static String base64(int decimal){
+        String str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String hashStr = "";
+        while(decimal > 0){
+            hashStr += str.charAt(decimal%62);
+            decimal /=62;
+        }
+        return  hashStr;
     }
 }
