a48ad7afd58048ecd6be6bcb391f4501303e7de9
Yushu <91983374+Yushuser@users.noreply.github.com>
2022-03-26 16:26:35 -0700

    Delete AddOperation.java

--- Mid-term/AddOperation.java
+++
@@ -1,8 +0,0 @@
-package edu.northeastern.yushu;
-
-public class AddOperation implements MathOperation{
-    @Override
-    public int performOperation(int num1, int num2) {
-        return num1 + num2;
-    }
-}
