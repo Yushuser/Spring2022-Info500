beaeb1d6509d450356c6e932b5e387d4dad6cf22
Yushu <91983374+Yushuser@users.noreply.github.com>
2022-03-26 16:27:22 -0700

    Delete SubtractOperation.java

--- Mid-term/SubtractOperation.java
+++
@@ -1,8 +0,0 @@
-package edu.northeastern.yushu;
-
-public class SubtractOperation implements MathOperation{
-    @Override
-    public int performOperation(int num1, int num2) {
-        return num1 - num2;
-    }
-}
