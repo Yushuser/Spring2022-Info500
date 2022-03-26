aef8953559c2bab41c5c6a5f56c4134ca81ef6fd
yushuwu <wu.yushu1@northeastern.edu>
2022-03-26 15:04:28 -0700

    Question 2

---
+++ Mid-term/Q2/src/edu/northeastern/yushu/Main.java
@@ -0,0 +1,25 @@
+package edu.northeastern.yushu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Question 2 Write difference between abstract class and interface.
+        Interface:
+        The interface is a blueprint that can be used to implement a class.
+        The interface does not contain any concrete methods (methods that have code).
+        All the methods of an interface are abstract methods.
+        An interface cannot be instantiated.
+        However, classes that implement interfaces can be instantiated.
+        Interfaces never contain instance variables but, they can contain public static final variables (i.e., constant class variables)
+        Abstract class:
+        A class which has the abstract keyword in its declaration is called abstract class.
+        Abstract classes should have at least one abstract method. , i.e., methods without a body.
+        It can have multiple concrete methods.
+        Abstract classes allow you to create blueprints for concrete classes.
+        But the inheriting class should implement the abstract method.Abstract classes cannot be instantiated.
+        Difference:
+        An abstract class permits you to make functionality that subclasses can implement or override whereas an interface only permits you to state functionality but not to implement it.
+        A class can extend only one abstract class while a class can implement multiple interfaces.
+        */
+    }
+}
