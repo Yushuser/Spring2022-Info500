9edd53333c77ca5f2805d5e279d9456bb9f553bf
yushuwu <wu.yushu1@northeastern.edu>
2022-03-28 15:35:20 -0700

    Assignment4

--- Assignment4/src/edu/northeastern/yushu/Main.java
+++ Assignment4/src/edu/northeastern/yushu/Main.java
@@ -8,26 +8,34 @@ public class Main {
     }
 }
 class Queue<T> {
+    private Object lock;
     Stack<T> s1;
-    Stack<T> s2;
     Queue() {
         s1 = new Stack<>();
-        s2 = new Stack<>();
+
     }
     // 1.void add(T data)
     public void add(T data) {
-
+        synchronized (lock) {
+            this.s1.push(data);
+        }
     }
     // 2. T remove()
     public T remove() {
-
+        synchronized (lock) {
+            return this.s1.pop();
+        }
     }
     // 3. T peek()
     public T peek() {
-
+        synchronized (lock) {
+            return this.s1.peek();
+        }
     }
     // 4. boolean isEmpty()
     public boolean isEmpty() {
-
+        synchronized (lock) {
+            return s1.peek() == null;
+        }
     }
 }
